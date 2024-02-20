43223a977496681a719f7a1bf3298407f389ec85
FFreezer <111958266+fly340@users.noreply.github.com>
2024-02-20 19:53:01 +0800

    Add files via upload

---
+++ Collection/test.cpp
@@ -0,0 +1,14 @@
+//计算两数之和。
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	scanf("%d %d", &a, &b);
+	int sum = a + b;
+	printf("a+b=%d", sum);
+
+	return 0;
+}
