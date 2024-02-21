3a605f3539898f934ebc21a1f19758fc915bdc2a
FFreezer <111958266+fly340@users.noreply.github.com>
2024-02-21 15:03:15 +0800

    Add files via upload

---
+++ 20240221_4.cpp
@@ -0,0 +1,15 @@
+﻿//计算三个小数的平均值。
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main()
+{
+	double a, b, c;
+	scanf("%lf %lf %lf", &a, &b, &c);
+	double d = (a + b + c) / 3;
+	d = (d * 100 + 5) / 100;
+	printf("平均值为%.1f", d);
+
+	
+	return 0;
+}
