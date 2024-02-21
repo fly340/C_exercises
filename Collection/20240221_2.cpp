81fd4e61da3ffd6ac21f51a8896675f4809a7ba5
FFreezer <111958266+fly340@users.noreply.github.com>
2024-02-21 13:48:30 +0800

    Add files via upload

---
+++ Collection/20240221_2.cpp
@@ -0,0 +1,13 @@
+﻿//把560分钟换算成用小时和分钟表示并输出。
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main()
+{
+	int a = 560;
+	int m = 560 % 60;
+	int h = (a - m) / 60;
+	printf("560 minutes equals %d hours and %d minutes.", h, m);
+
+	return 0;
+}
