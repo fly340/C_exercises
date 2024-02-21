d4c73377a0f3820f3c9020d9e1c81a73120fb556
FFreezer <111958266+fly340@users.noreply.github.com>
2024-02-21 13:51:20 +0800

    Add files via upload

---
+++ 20240221.cpp
@@ -0,0 +1,16 @@
+﻿//计算长方形的面积和体积。
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main()
+{
+	double a, b, c, s, v;
+	printf("input a, b, c:\n");
+	scanf("%lf %lf %lf", &a, &b, &c);
+	s = a * b;                   /*计算长方形面积*/
+	v = a * b * c;               /*计算长方体体积*/
+	printf("a=%.6f,b=%.6f,c=%.6f\n", a, b, c);
+	printf("s=%.6f,v=%.6f\n", s, v);
+
+	return 0;
+}
